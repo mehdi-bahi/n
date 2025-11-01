0db89455f1b742632d75573e51e1e2ca0e12ea9d
mehdi-bahi <107451627+mehdi-bahi@users.noreply.github.com>
2025-11-01 16:27:43 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
           to="/"
           className="text-2xl font-bold font-playfair text-gold hover:text-gold/80 transition-smooth"
         >
-          Salon Escal Souad SPA
+          Golf Stinia
         </Link>
 
         {/* Desktop Navigation */}
